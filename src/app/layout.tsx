e38f54a698504ffe570171bc21a3b293e6d89b8e
Tan Tran <tantran.300803@gmail.com>
2025-07-04 00:21:57 +0700

    Add Rocket.Chat live chat integration script to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Suspense, type ReactNode } from "react";
 import { type Metadata } from "next";
 import { DraftModeNotification } from "@/ui/components/DraftModeNotification";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,18 @@ export default function RootLayout(props: { children: ReactNode }) {
 				<Suspense>
 					<DraftModeNotification />
 				</Suspense>
+				<Script id="rocketchat-livechat" strategy="afterInteractive">
+					{`
+            (function(w, d, s, u) {
+                w.RocketChat = function(c) { w.RocketChat._.push(c) }; w.RocketChat._ = []; w.RocketChat.url = u;
+                var h = d.getElementsByTagName(s)[0], j = d.createElement(s);
+                j.async = true; j.src = 'https://livechat.jarvis.cx/livechat/rocketchat-livechat.min.js?_=201903270000';
+                h.parentNode.insertBefore(j, h);
+                w.ticketplus = w.ticketplus || {};
+                w.ticketplus.tenantid = '51bd39ec-1ad8-4aad-b130-85e212b9fe58';
+            })(window, document, 'script', 'https://livechat.jarvis.cx/livechat');
+          `}
+				</Script>
 			</body>
 		</html>
 	);
